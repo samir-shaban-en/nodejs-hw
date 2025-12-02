719a51c3d35aebe7bd73bb0d869f03d182ede3c7
Samir Shaban <samirshabanpsn@gmail.com>
2025-12-02 19:12:30 +0100

    do hm

---
+++ src/server.js
@@ -0,0 +1,45 @@
+import 'dotenv/config';
+import cors from 'cors';
+import pinoHttp from 'pino-http';
+import express from 'express';
+
+const PORT = process.env.PORT ?? 3000;
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+const logger = pinoHttp();
+app.use(logger);
+
+app.get('/notes', (req, res) => {
+  req.log.info('Отримання усіх нотаток');
+  res.status(200).json({ message: 'Retrieved all notes' });
+});
+
+app.get('/notes/:noteId', (req, res) => {
+  const { noteId } = req.params;
+  req.log.info(`Отримання нотатки з ID: ${noteId}`);
+  res.status(200).json({
+    message: `Retrieved note with ID: ${noteId}`,
+  });
+});
+
+app.get('/test-error', (req, res) => {
+  throw new Error('Simulated server error');
+});
+
+app.use((req, res) => {
+  res.status(404).json({ message: 'Route not found' });
+});
+
+app.use((err, req, res, next) => {
+  res.status(500).json({
+    message: `${err}`,
+  });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
