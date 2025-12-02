3cd0c9b076e1a0da2118171e4bcbfb0270db27bc
Samir Shaban <samirshabanpsn@gmail.com>
2025-12-02 19:18:13 +0100

    do hm

--- src/server.js
+++ src/server.js
@@ -40,6 +40,18 @@ app.use((err, req, res, next) => {
   });
 });
 
+app.use((err, req, res, next) => {
+  console.error(err);
+
+  const isProd = process.env.NODE_ENV === 'production';
+
+  res.status(500).json({
+    message: isProd
+      ? 'Something went wrong. Please try again later.'
+      : err.message,
+  });
+});
+
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
