ba575746d0846b51ebb4547785523bdae9983721
Samir Shaban <samirshabanpsn@gmail.com>
2025-12-03 12:39:37 +0100

    delete one middleware

--- src/server.js
+++ src/server.js
@@ -54,18 +54,6 @@ app.use((err, req, res, next) => {
   });
 });
 
-app.use((err, req, res, next) => {
-  console.error(err);
-
-  const isProd = process.env.NODE_ENV === 'production';
-
-  res.status(500).json({
-    message: isProd
-      ? 'Something went wrong. Please try again later.'
-      : err.message,
-  });
-});
-
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
