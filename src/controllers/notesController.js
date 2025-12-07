39be99535608cdc57f8698f1343da14c345827d3
Samir Shaban <samirshabanpsn@gmail.com>
2025-12-07 18:02:41 +0100

    do hm

--- src/controllers/notesController.js
+++ src/controllers/notesController.js
@@ -18,3 +18,37 @@ export const getNoteById = async (req, res, next) => {
 
   res.status(200).json(note);
 };
+
+export const createNote = async (req, res) => {
+  const note = await Note.create(req.body);
+  res.status(201).json(note);
+};
+
+export const deleteNote = async (req, res, next) => {
+  const { noteId } = req.params;
+  const note = await Note.findOneAndDelete({
+    _id: noteId,
+  });
+
+  if (!note) {
+    next(createHttpError(404, 'Note not found'));
+    return;
+  }
+
+  res.status(200).json(note);
+};
+
+export const updateNote = async (req, res, next) => {
+  const { noteId } = req.params;
+
+  const note = await Note.findOneAndUpdate({ _id: noteId }, req.body, {
+    new: true,
+  });
+
+  if (!note) {
+    next(createHttpError(404, 'Note not found'));
+    return;
+  }
+
+  res.status(200).json(note);
+};
