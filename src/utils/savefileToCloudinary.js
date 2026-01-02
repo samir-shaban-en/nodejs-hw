37241de55dbff643c4f4eb8b3444cacbe63ff135
Samir Shaban <samirshabanpsn@gmail.com>
2026-01-02 22:01:38 +0100

    fix

--- src/utils/savefileToCloudinary.js
+++ src/utils/savefileToCloudinary.js
@@ -7,3 +7,20 @@ cloudinary.config({
   api_key: process.env.CLOUDINARY_API_KEY,
   api_secret: process.env.CLOUDINARY_API_SECRET,
 });
+
+export async function saveFileToCloudinary(buffer, userId) {
+  return new Promise((resolve, reject) => {
+    const uploadStream = cloudinary.uploader.upload_stream(
+      {
+        folder: 'students-app/avatars',
+        resource_type: 'image',
+        public_id: `avatar_${userId}`,
+        overwrite: true,
+        unique_filename: false,
+      },
+      (err, result) => (err ? reject(err) : resolve(result)),
+    );
+
+    Readable.from(buffer).pipe(uploadStream);
+  });
+}
